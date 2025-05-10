129c3705e955e4877dba33f18e4e8f83b4e66fba
CRISTINA SARAIVA FIGUEIREDO <kristinasaraiva12@gmail.com>
2025-05-09 21:26:46 -0300

    criando responsabilidade no site (página principal e nossa história)

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const btn = document.getElementById('menu-btn');
-    const menu = document.querySelector('.menu-lateral');
+const menu = document.querySelector('.menu-lateral');
 
     btn.addEventListener('click', () => {
         menu.classList.toggle('ativo');
+
+        // Impede ou permite o scroll no body conforme o menu está ativo
+        if (menu.classList.contains('ativo')) {
+            document.body.style.overflow = 'hidden'; // trava o scroll
+        } else {
+            document.body.style.overflow = "auto"; // libera o scroll
+        }
     });
+
+
