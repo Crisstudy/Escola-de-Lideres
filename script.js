64bdf525aa07ecab167fa6ba0962734bf1d9a704
CRISTINA SARAIVA FIGUEIREDO <kristinasaraiva12@gmail.com>
2025-05-14 19:06:37 -0300

    adicionando elementos CSS a pagina Aulas

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ const menu = document.querySelector('.menu-lateral');
         menu.classList.toggle('ativo');
 
         // Impede ou permite o scroll no body conforme o menu está ativo
-        if (menu.classList.contains('ativo')) {
-            document.body.style.overflow = 'hidden'; // trava o scroll
-        } else {
-            document.body.style.overflow = "auto"; // libera o scroll
-        }
-    });
+    //     if (menu.classList.contains('ativo')) {
+    //         document.body.style.overflow = 'hidden'; // trava o scroll
+    //     } else {
+    //         document.body.style.overflow = "auto"; // libera o scroll
+    //     }
+     });
 
 
